73596f39e7431c7dba208ae47720f486f7ea2537
Mehrad <144580664+mikeesierrah@users.noreply.github.com>
2025-09-26 18:40:38 +0200

    Update ez-warp.sh

--- ez-warp.sh
+++ ez-warp.sh
@@ -64,7 +64,7 @@ rm -rf wgcf-account.toml &> /dev/null || true
 rm -rf /etc/wireguard/warp.conf &> /dev/null || true
 # main dish
 
-wgcf register
+wgcf register --accept-tos || true
 read -rp "Do you want to use your own key? (Y/n): " response
 if [[ $response =~ ^[Yy]$ ]]; then
     read -rp "ENTER YOUR LICENSE: " LICENSE_KEY
